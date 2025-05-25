11811fc0944a9eb35898e1559c5099779efc1b1d
fabriciofirmino <34897606+fabriciofirmino@users.noreply.github.com>
2025-05-25 17:52:25 -0300

    feat: Atualiza as regras de CORS para permitir métodos PATCH

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,7 +10,7 @@ import (
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Permite todas as origens (não recomendado em produção)
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if c.Request.Method == "OPTIONS" {
